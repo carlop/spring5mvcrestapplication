c0864ea1eb70a2c86df0244d393bdd24a408dd6d
Carlos López <yo@carlop.es>
2021-06-16 11:33:22 +0200

    Category Service

---
+++ src/main/java/es/carlop/services/CategoryService.java
@@ -0,0 +1,12 @@
+package es.carlop.services;
+
+import es.carlop.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
