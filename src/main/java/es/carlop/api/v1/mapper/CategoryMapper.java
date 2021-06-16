420770a8625b010621e5097f127c4379921618ef
Carlos López <yo@carlop.es>
2021-06-16 11:08:30 +0200

    MapStruct IntelliJ Plugin

--- src/main/java/es/carlop/api/v1/mapper/CategoryMapper.java
+++ src/main/java/es/carlop/api/v1/mapper/CategoryMapper.java
@@ -4,6 +4,7 @@ package es.carlop.api.v1.mapper;
 import es.carlop.api.v1.model.CategoryDTO;
 import es.carlop.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,5 +12,6 @@ public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
