58ade6bad044dded54e19b4fe3631d5101681a45
Carlos López <yo@carlop.es>
2021-06-21 18:29:05 +0200

    Customizing endpoint documentation.

--- src/main/java/es/carlop/api/v1/model/CustomerDTO.java
+++ src/main/java/es/carlop/api/v1/model/CustomerDTO.java
@@ -1,11 +1,14 @@
 package es.carlop.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
+    @Schema(description = "This is the first name.", required = true)
     private String firstName;
+    @Schema(description = "This is the last name.", required = true)
     private String lastName;
 
     @JsonProperty("customer_url")
--- src/main/java/es/carlop/controllers/v1/CustomerController.java
+++ src/main/java/es/carlop/controllers/v1/CustomerController.java
@@ -4,9 +4,12 @@ import es.carlop.BaseURLs;
 import es.carlop.api.v1.model.CustomerDTO;
 import es.carlop.api.v1.model.CustomersListDTO;
 import es.carlop.services.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "customer-controller", description = "This is the Customer controller")
 @RestController
 @RequestMapping(BaseURLs.CUSTOMERS_URL)
 public class CustomerController {
@@ -17,6 +20,8 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @Operation(summary = "This will get a list of customers.",
+            description = "These are some notes about the API.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomersListDTO getAllCustomers() {
